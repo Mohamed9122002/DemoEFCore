fd8735d8f89d296be025e41d7ba012635f8b9191
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-28 23:11:09 +0300

    Run Sql Raw

--- DataBaseFirst/Program.cs
+++ DataBaseFirst/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using DataBaseFirst.Contexts;
+using DataBaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataBaseFirst
 {
@@ -15,12 +17,25 @@ namespace DataBaseFirst
             ///Scaffold-DbContext -Connection "Server=.; Database = Northwind; Trusted_Connection = true; TrustServerCertificate = true"  Microsoft.EntityFrameworkCore.SqlServer -Context "MyNorthWindDbContext" -ContextDir "Contexts" -OutputDir "Models"  -Tables "Customers",Employees",Orders","Products", "Sales Totals by Amount"
             using MyNorthwindDbContext
                 myNorthWindDbContext = new MyNorthwindDbContext();
-            var customers = myNorthWindDbContext.Customers.ToList();
-            foreach (var customer in customers)
-            {
-                Console.WriteLine($"{customer.CustomerId} {customer.CompanyName} {customer.ContactName}");
-            }
+            //var customers = myNorthWindDbContext.Customers.ToList();
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine($"{customer.CustomerId} {customer.CompanyName} {customer.ContactName}");
+            //}
+            #region Select Statment 
+            //var result = myNorthWindDbContext.Products.FromSqlRaw("Select * from Products Where CategoryId = 1 ");
+            //int catId = 1;
+            //var re = myNorthWindDbContext.Products.FromSqlInterpolated($"Select * from Products Where CategoryId ={catId}");
+            //foreach (var item in re)
+            //{
+            //    Console.WriteLine($" {item.ProductName}");
+            //} 
+            #endregion
+            #region DML 
+            //ExecuteSqlRaw
+            //ExecuteSqlInterpolated
 
+            #endregion
         }
     }
 }
