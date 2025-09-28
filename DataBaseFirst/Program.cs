91d417996a174bdde6447c73aeceaae0f6156288
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-28 22:19:00 +0300

    Database First [Command]

---
+++ DataBaseFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using DataBaseFirst.Contexts;
+
+namespace DataBaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Install EF Core packages
+            //2.Scaffold the models and DbContext
+
+            //Scaffold-DbContext -Connection "Server=.; Database = Northwind; Trusted_Connection = true; TrustServerCertificate = true" -Provider
+            ///Scaffold-DbContext -Connection "Server=.; Database = Northwind; Trusted_Connection = true; TrustServerCertificate = true"  Microsoft.EntityFrameworkCore.SqlServer -Context "MyNorthWindDbContext" -ContextDir "Contexts" -OutputDir "Models"  -Tables "Customers",Employees",Orders","Products", "Sales Totals by Amount"
+            //using MyNorthWindDbContext myNorthWindDbContext = new MyNorthWindDbContext();
+            //var customers = myNorthWindDbContext.Customers.ToList();
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine($"{customer.CustomerId} {customer.CompanyName} {customer.ContactName}");
+            //}
+
+        }
+    }
+}
