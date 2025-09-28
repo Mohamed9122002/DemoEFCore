8ed8afbede5909aa6a85138bb11d7c9c6ac65c1e
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-28 18:43:25 +0300

    Eager Loading

--- DemoEFCore/Program.cs
+++ DemoEFCore/Program.cs
@@ -27,12 +27,55 @@ namespace DemoEFCore
             //companyDb.Add(employee01);
             //companyDb.SaveChanges();    
             #region Dynmaic Data Seeding 
-         bool flag =  CompanyDbContextSeed.DataSeeding(companyDb);
-            if(flag)
-                Console.WriteLine("Data Seeded Successfully");
-            else
-                Console.WriteLine("Data Seeding Failed");
+            //bool flag =  CompanyDbContextSeed.DataSeeding(companyDb);
+            //   if(flag)
+            //       Console.WriteLine("Data Seeded Successfully");
+            //   else
+            //       Console.WriteLine("Data Seeding Failed");
 
+            #endregion
+            #region Navigation Property - Related Data 
+            //var emp01 = companyDb.Employees.FirstOrDefault(e => e.EmpId == 25);
+            //if (emp01 != null)
+            //{
+            //    Console.WriteLine($"Employee Name : {emp01.EmpName}");
+            //    Console.WriteLine($"Employee Salary : {emp01.Salary}");
+            //    Console.WriteLine($"Employee Age : {emp01.EmployeeDepartment.DeptName}"); // Realted Data 
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee Not Found");
+            //}
+            #endregion
+            #region Eager Loading 
+            //var emp01 = companyDb.Employees.Include(E=>E.EmployeeDepartment).FirstOrDefault(e => e.EmpId == 25);
+            //if (emp01 != null)
+            //{
+            //    Console.WriteLine($"Employee Name : {emp01.EmpName}");
+            //    Console.WriteLine($"Employee Department : {emp01.EmployeeDepartment.DeptName}"); 
+            //}
+            ////// if Navigation Property is Mandatory [Not Null] ==> Inner Join 
+            ///// if Navigation Property is Optional [Null] ==> Left Join 
+            //else
+            //{
+            //    Console.WriteLine("Employee Not Found");
+            //}
+            //var empwithManagerDepartment = companyDb.Employees.Include(E => E.ManageDepartment).FirstOrDefault(e => e.EmpId == 25);
+            //if (empwithManagerDepartment != null)
+            //{
+            //    Console.WriteLine($"Employee Name : {empwithManagerDepartment.EmpName}");
+            //    Console.WriteLine($"Employee Department : {empwithManagerDepartment.ManageDepartment?.DeptName ?? "No Department"}");
+            //}
+
+            //var empwithDepartment = companyDb.Employees.Include(E => E.EmployeeDepartment).ThenInclude(D=>D.Manager).FirstOrDefault(e => e.EmpId == 25);
+            //if (empwithDepartment is not null)
+            //{
+            //    Console.WriteLine($"Employee Name : {empwithDepartment.EmpName}");
+            //    Console.WriteLine($"Employee Department : {empwithDepartment.EmployeeDepartment?.DeptName}");
+            //    Console.WriteLine(empwithDepartment.EmployeeDepartment?.Manager?.EmpId);
+            //    Console.WriteLine(empwithDepartment.EmployeeDepartment?.Manager?.EmpName);
+            //}
+            // eager Loading => One request => All Data
             #endregion
 
         }
